8626864ea92aba44685ac45647fc13ad7a005292
alishba2 <alishbashazadi79@gmail.com>
2024-11-17 14:56:26 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+
+
+
+
+// callback function
+
+// when async operation gets completed
+
+app.listen(3000, () => {
+
+    console.log("Server running at port 3000");
+})
